a91e6065d4ae3ab911ebe91e6ad25e4124503b65
Leslie Figueroa <lesliek.x1999@gmail.com>
2026-05-23 21:04:28 -0400

    mejora boton scroll suave

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
-// Función para volver al inicio de la página suavemente
+// Selecciona el botón
+const btnArriba = document.getElementById("btn-arriba");
+
+// Muestra u oculta el botón según el scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    btnArriba.classList.add("visible");
+  } else {
+    btnArriba.classList.remove("visible");
+  }
+});
+
+// Función para volver al inicio suavemente
 function irArriba() {
   window.scrollTo({
     top: 0,
